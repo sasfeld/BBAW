c14161b65dca6125544749e7c2706738fae80c1a
Sascha Feldmann <wsp-shk@bbaw.de>
2012-06-29 17:30:49 +0200

    * Added constants for many file extensions that are currently supported. * Added a method to fetch a file extension. * Added a method to determine whether an extension represents an image.

--- src/bbaw/wsp/crawler/accepter/ResourceAccepter.java
+++ src/bbaw/wsp/crawler/accepter/ResourceAccepter.java
@@ -12,6 +12,35 @@ import java.util.Set;
  */
 public abstract class ResourceAccepter {
 
+	/**
+	 * PDF file extension.
+	 */
+	public static final Object EXT_PDF = ".pdf";
+	/**
+	 * DOC file extension.
+	 */
+	public static final Object EXT_DOC = ".doc";
+	/**
+	 * Open document text extension.
+	 */
+	public static final Object EXT_ODT = ".odt";
+	/**
+	 *  XML extension.
+	 */
+	public static final Object EXT_XML = ".xml";
+	/**
+	 *  JPG extension.
+	 */
+	private static final Object EXT_JPG = ".jpg";
+	/**
+	 *  TIFF extension.
+	 */
+	private static final Object EXT_TIFF = ".tiff";
+	/**
+	 *  PNG extension.
+	 */
+	private static final Object EXT_PNG = ".png";
+	
 	protected Set<String> acceptedResources;
 
 	public ResourceAccepter(final Set<String> acceptedResources) {
@@ -38,5 +67,27 @@ public abstract class ResourceAccepter {
 	 * @return ture if the node-resource is accepted
 	 */
 	public abstract boolean acceptNode(String uri);
+	
+	/**
+	 * Fetch the URI's extension.
+	 */
+	public String getExtension(final String uri) {
+		final int extPos = uri.lastIndexOf(".");
+		final String extension = uri.substring(extPos, uri.length());
+		
+		return extension;
+	}
+	
+	/**
+	 * Check if the resource is an image.
+	 * @param uri - the resource's URI.
+	 * @return ture if the resource is an image.
+	 */
+	public boolean isImage(String uri) {		
+		if(getExtension(uri).equals(EXT_JPG) || getExtension(uri).equals(EXT_TIFF)|| getExtension(uri).equals(EXT_PNG)) {
+			return true;
+		}
+		return false;
+	}
 	
 }
