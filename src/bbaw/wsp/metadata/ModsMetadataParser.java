f0952f56ccc30214cc7e5a2eac0ddc77a13d374c
Sascha Feldmann <wsp-shk@bbaw.de>
2012-07-26 14:33:52 +0200

    * Switched from XPath to Saxon - S9 API.

--- src/bbaw/wsp/metadata/ModsMetadataParser.java
+++ src/bbaw/wsp/metadata/ModsMetadataParser.java
@@ -1,13 +1,18 @@
 package bbaw.wsp.metadata;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.File;
 
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.xpath.XPath;
-
+import net.sf.saxon.om.ValueRepresentation;
+import net.sf.saxon.s9api.DocumentBuilder;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XdmValue;
+import net.sf.saxon.trans.XPathException;
 import bbaw.wsp.crawler.tools.LogFile;
 
 /**
@@ -17,10 +22,9 @@ import bbaw.wsp.crawler.tools.LogFile;
  * @author Sascha Feldmann (wsp-shk1)
  * 
  */
-@SuppressWarnings("deprecation")
 public class ModsMetadataParser {
-
-	private Object doc;
+	private XPathCompiler xPathCompiler;
+	private XdmNode contextItem;
 
 	/**
 	 * Create a new ModsMetadataParser instance.
@@ -36,21 +40,23 @@ public class ModsMetadataParser {
 			throw new IllegalArgumentException(
 					"The value for the parameter uri in the constructor of ModsMetadataParser mustn't be empty.");
 		}
-		FileInputStream in;
-		try {
-			in = new FileInputStream(uri);
-			SAXBuilder builder = new SAXBuilder();
-			Document doc = builder.build(uri);
-			in.close();
-			this.doc = doc;
-		} catch (IOException | JDOMException e) {
-			e.printStackTrace();
-		}
 
+		Processor processor = new Processor(false);
+		xPathCompiler = processor.newXPathCompiler();
+		xPathCompiler.declareNamespace("mods", "http://www.loc.gov/mods/v3");
+		DocumentBuilder builder = processor.newDocumentBuilder();
+		try {
+			contextItem = builder.build(new File(uri));
+		} catch (SaxonApiException e) {
+			LogFile.writeLog("Problem while parsing MODS-file "
+					+ this.xPathCompiler.getBaseURI() + "  -- exception: " + e.getMessage()
+					+ "\n");
+		}
 	}
 
 	/**
 	 * Parse the record and return an {@link WSPMetadataRecord} object.
+	 * 
 	 * @return an {@link WSPMetadataRecord} object.
 	 */
 	public WSPMetadataRecord parse() {
@@ -61,51 +67,53 @@ public class ModsMetadataParser {
 
 		final String url = parseURL();
 		modsObject.setUrl(url);
-		
+
 		final String title = parseTitle();
 		modsObject.setTitle(title);
-		
+
 		final String mods_abstract = parseAbstract();
 		modsObject.setMods_abstract(mods_abstract);
 
 		final String publisher = parsePublisher();
 		modsObject.setPublisher(publisher);
-		
+
 		final String dateIssued = parseDateIssued();
 		modsObject.setDateIssued(dateIssued);
-		
-		
+
 		final String[] persIds = parsePersonalIds();
 		modsObject.setPersonals(persIds);
-		
+
 		final String placeTerm = parsePlaceTerm();
 		modsObject.setPlaceTerm(placeTerm);
-		
+
 		final String[] topics = parseTopics();
 		modsObject.setTopics(topics);
-		
+
 		final String[] geographics = parseGeographics();
 		modsObject.setGeographics(geographics);
 
 		final String temporalStart = parseTemporalStart();
 		modsObject.setTemporalStart(temporalStart);
-		
+
 		final String temporalEnd = parseTemporalEnd();
 		modsObject.setTemporalEnd(temporalEnd);
-		
+
 		return modsObject;
-	}	
-	
+	}
+
 	private String parseTemporalEnd() {
-		return (String) buildXPath("//mods:subject/mods:temporal[@point='end']/text()", false);
+		return (String) buildXPath(
+				"//mods:subject/mods:temporal[@point='end']/text()", false);
 	}
 
 	private String parseTemporalStart() {
-		return (String) buildXPath("//mods:subject/mods:temporal[@point='start']/text()", false);
+		return (String) buildXPath(
+				"//mods:subject/mods:temporal[@point='start']/text()", false);
 	}
 
 	private String[] parseGeographics() {
-		return (String[]) buildXPath("//mods:subject/mods:geographic/text()", true);
+		 return (String[]) buildXPath("//mods:subject/mods:geographic/text()",
+		 true);		
 	}
 
 	private String[] parseTopics() {
@@ -113,11 +121,13 @@ public class ModsMetadataParser {
 	}
 
 	private String parseDateIssued() {
-		return (String) buildXPath("//mods:originInfo/mods:dateIssued/text()", false);
+		return (String) buildXPath("//mods:originInfo/mods:dateIssued/text()",
+				false);
 	}
 
 	private String parsePublisher() {
-		return (String) buildXPath("//mods:originInfo/mods:publisher/text()", false);
+		return (String) buildXPath("//mods:originInfo/mods:publisher/text()",
+				false);
 	}
 
 	private String parseAbstract() {
@@ -129,7 +139,8 @@ public class ModsMetadataParser {
 	}
 
 	private String parsePlaceTerm() {
-		return (String) buildXPath("//mods:originInfo/mods:place/mods:placeTerm/text()", false);
+		return (String) buildXPath(
+				"//mods:originInfo/mods:place/mods:placeTerm/text()", false);
 	}
 
 	private String parseID() {
@@ -137,57 +148,54 @@ public class ModsMetadataParser {
 	}
 
 	private String[] parsePersonalIds() {
-		return (String[]) buildXPath("//mods:name[@type='personal']/@ID", true);
+		 return (String[]) buildXPath("//mods:name[@type='personal']/@ID",
+		 true);		
 	}
 
 	private String parseURL() {
 		return (String) buildXPath("//mods:url/text()", false);
 	}
 
-	@SuppressWarnings("deprecation")
 	private Object buildXPath(final String query, final boolean moreNodes) {
-		XPath xPath;
 		try {
-			xPath = XPath.newInstance(query);
-			xPath.addNamespace("mods", "http://www.loc.gov/mods/v3");
+			XPathExecutable x = xPathCompiler.compile(query);
 
+			XPathSelector selector = x.load();
+			selector.setContextItem(this.contextItem);
+			XdmValue value = selector.evaluate();
 			if (moreNodes) {
-				Object results = xPath.selectNodes(this.doc);
-
-				String[] persIds = ModsHelper.getStringToArray(results);
-				return persIds;
-			} else {
-				Object result = xPath.selectSingleNode(this.doc);
-				
-				if(result != null) {
-				return ModsHelper.removeCtrlChars(result.toString());
+				String[] list = new String[value.size()];
+				int i = 0;
+				for (XdmItem xdmItem : value) {					
+					list[i] = xdmItem.toString();
+					++i;
 				}
-			}
-		} catch (JDOMException | IllegalArgumentException e) {
-			LogFile.writeLog("Problem while parsing MODS-file " + this.doc.toString()
-					+ "  -- exception: " + e.getMessage() + "\n");
-			if(moreNodes) {
-				String[] ret = {""}; 
-				return ret;
+				// Replace attribute chars
+				return ModsHelper.removeAttributeChars(list);
 			}
 			
+			
+			ValueRepresentation rep = value.getUnderlyingValue();
+			// Replace attribute chars
+			return ModsHelper.removeAttributeChars(rep.getStringValue());		
+		} catch (SaxonApiException | XPathException e) {
+			LogFile.writeLog("Problem while parsing MODS-file " +
+					 this.xPathCompiler.getBaseURI()
+					 + "  -- exception: " + e.getMessage() + "\n");
 			return "";
 		}
-		
-		return "";
-
 	}
 
 	public static void main(String[] args) {
-//		String url = "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090714-2-WS-BBAW.xml";
-//		String url = "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090709-1-WS-BBAW.xml";
-		String url = "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090716-5-WS-BBAW.xml";
-		ModsMetadataParser parser = new ModsMetadataParser(
-				url);
+		// String url =
+		// "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090714-2-WS-BBAW.xml";
+		 String url =
+		 "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090709-1-WS-BBAW.xml";
+//		String url = "C:/Dokumente und Einstellungen/wsp-shk1/Eigene Dateien/eXist-wsp/db/wsp/dataMods/20090716-5-WS-BBAW.xml";
+		ModsMetadataParser parser = new ModsMetadataParser(url);
 		WSPMetadataRecord obj = parser.parse();
 
 		System.out.println(obj);
-	
 
 	}
 
